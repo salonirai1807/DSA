47ddd01f9eac4ead27fc8f061fda45c080d50672
Saloni Rai <salonirai922@gmail.com>
2024-02-02 12:55:01 +0530

    Pattern #6 & #7

--- Patterns/p3.cpp
+++ Patterns/p3.cpp
@@ -17,8 +17,8 @@ int main() {
     i1=1;
     while(i1<=n1) {
         j1=1;
-        while (j1<=n1) {
-            cout<<j1<<" ";
+        while (j1<=i1) {
+            cout<<"*";
             j1=j1+1;
         }
         cout<<endl;
@@ -26,17 +26,23 @@ int main() {
         i1=i1+1;        
     }
 
+/* Pattern #7
+
+1
+2 2
+3 3 3
+4 4 4 4    */
 
     int i2, j2, n2;
 
-    cout<<"Enter No. of Rows & Columns : ";
+    cout<<"Enter No. of Rows : ";
     cin>>n2;
 
     i2=1;
     while(i2<=n2) {
         j2=1;
-        while (j2<=n2) {
-            cout<<n2-j2+1<<" ";
+        while (j2<=i2) {
+            cout<<i2<<" ";
             j2=j2+1;
         }
         cout<<endl;
